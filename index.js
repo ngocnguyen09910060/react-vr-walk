04de5aa74487274b3579aa8f2ba7b3f70d583fd7
Apostolis Economou <apoeco@users.noreply.github.com>
2017-05-20 00:48:47 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import {
+  Scene,
+  Animated,
+} from 'react-vr';
+
+const AnimatedScene = Animated.createAnimatedComponent(Scene);
+
+export default class Walk extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      x: new Animated.Value(0),
+      z: new Animated.Value(0),
+    }
+    this.moveZ = Animated.event([
+      null, { dz: this.state.z },
+    ])
+    this.moveX = Animated.event([
+      null, { dx: this.state.x },
+    ])
+
+    this.positionZ = 0
+    this.positionX = 0
+
+  }
+  onInput(e) {
+    if (e.nativeEvent.inputEvent.eventType == 'keydown') {
+      switch (e.nativeEvent.inputEvent.code) {
+        case 'KeyW':
+          this.walk({position: 'w'})
+          break
+        case 'KeyA':
+          this.walk({position: 'a'})
+          break
+        case 'KeyS':
+          this.walk({position: 's'})
+          break
+        case 'KeyD':
+          this.walk({position: 'd'})
+          break
+        default:
+          break
+      }
+    }
+  }
+  walk({position}) {
+    switch (position) {
+      case 'w':
+        this.positionZ = this.positionZ - 0.1;
+        this.moveZ(null, { dz: this.positionZ });
+        break
+      case 'a':
+        this.positionX = this.positionX + 0.1;
+        this.moveX(null, { dx: this.positionX });
+        console.log('a');
+        break
+      case 's':
+        this.positionZ = this.positionZ + 0.1;
+        this.moveZ(null, { dz: this.positionZ });
+        break
+      case 'd':
+        this.positionX = this.positionX - 0.1;
+        this.moveX(null, { dx: this.positionX });
+        break
+      default:
+        break
+    }
+  }
+  render() {
+    return (
+      <AnimatedScene onInput={(e) => this.onInput(e)} style={{ transform: [{translateZ: this.state.z}, {translateX: this.state.x }] }}>
+        {this.props.children}
+      </AnimatedScene>
+    );
+  }
+};
